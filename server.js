7c67b5c90040003bb14cfbce6feb2b5a1d3ebc3a
tonynater <tong.t.wang@gmail.com>
2016-03-25 22:48:54 -0700

    Setup basic socket.io

--- server.js
+++ server.js
@@ -5,6 +5,37 @@ var io = require('socket.io').listen(server);
 
 app.use('/', express.static('static/public'));
 
+function makeid() {
+    var possible = 
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var text = "";
+    for (var i = 0; i < 7; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+var sockets = {};
+io.on('connection', function(socket) {
+  var id;
+  do {
+    id = makeid();
+  } while(sockets[id]);
+
+  console.log('user connected with id: ' + id);
+
+  socket.on('score', function(score) {
+    console.log(score);
+    socket.broadcast.emit('score', score);
+  });
+
+  socket.on('disconnect', function() {
+    sockets[socket] = undefined;
+    delete sockets[socket];
+    console.log('user disconnected with id: ' + id);
+  });
+});
+
 server.listen(4200, function(){
   console.log('listening on *: 4200');
 });
