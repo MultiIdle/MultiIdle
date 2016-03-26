ad44433a6d007b303c2886912347e3f37d2fc4b3
tonynater <tong.t.wang@gmail.com>
2016-03-26 16:37:13 -0700

    Fixed names

--- static/public/js/game.js
+++ static/public/js/game.js
@@ -15,20 +15,20 @@ $.blockUI({ message: '<h1>Waiting for opponent...</h1><textarea id="link" style=
 var socket = io();
 
 var buildings = [
-  { name : "Hacker", base : 15, power: .1 },
-  { name : "Grandma", base : 100, power: 1 },
-  { name : "Farm", base : 1100, power: 8 },
+  { name : "Iron", base : 15, power: .1 },
+  { name : "Big Iron", base : 100, power: 1 },
+  { name : "Oh, the Irony", base : 1100, power: 8 },
   { name : "Mine", base : 12000, power: 47 },
   { name : "Factory", base : 130000, power: 260 },
   { name : "Bank", base : 1400000, power: 1400 },
   { name : "Temple", base : 20000000, power: 7800 },
-  { name : "Wizard Tower", base : 330000000, power: 44000 },
-  { name : "Shipment", base : 5100000000, power: 260000 },
-  { name : "Alchemy Lab", base : 75000000000, power: 1600000 },
-  { name : "Portal", base : 1000000000000, power: 10000000 },
-  { name : "Time Machine", base : 14000000000000, power: 65000000 },
-  { name : "Antimatter Condenser", base : 170000000000000, power: 430000000 },
-  { name : "Prism", base : 2100000000000000, power: 2900000000}
+  { name : "Pickle Factory", base : 330000000, power: 44000 },
+  { name : "The WALL", base : 5100000000, power: 260000 },
+  { name : "Munitions Depot", base : 75000000000, power: 1600000 },
+  { name : "Kyoto Animation", base : 1000000000000, power: 10000000 },
+  { name : "Quaternion Distillery", base : 14000000000000, power: 65000000 },
+  { name : "Big Black Hole", base : 170000000000000, power: 430000000 },
+  { name : "Grassy Magnetic Field Reversal^2", base : 2100000000000000, power: 2900000000}
 ];
 
 var consumables = [
