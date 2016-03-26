266c673e8aa8fe8d1d8c21dd286c807fda363ff4
tonynater <tong.t.wang@gmail.com>
2016-03-26 04:27:07 -0700

    Fixed building formatting

--- game/game.js
+++ game/game.js
@@ -1,76 +1,20 @@
-var buildings = 
-    [
-        {
-          name : "Hacker",
-          base : 15,
-          power: .1
-        },
-        {
-          name : "Grandma",
-          base : 100,
-          power: 1
-        },
-        {
-          name : "Farm",
-          base : 1100,
-          power: 8
-        },
-        {
-          name : "Mine",
-          base : 12000,
-          power: 47
-        },
-        {
-          name : "Factory",
-          base : 130000,
-          power: 260
-        },
-        {
-          name : "Bank",
-          base : 1400000,
-          power: 1400
-        },
-        {
-          name : "Temple",
-          base : 20000000,
-          power: 7800
-        },
-        {
-          name : "Wizard Tower",
-          base : 330000000,
-          power: 44000
-        },
-        {
-          name : "Shipment",
-          base : 5100000000,
-          power: 260000
-        },
-        {
-          name : "Alchemy Lab",
-          base : 75000000000,
-          power: 1600000
-        },
-        {
-          name : "Portal",
-          base : 1000000000000,
-          power: 10000000
-        },
-        {
-          name : "Time Machine",
-          base : 14000000000000,
-          power: 65000000
-        },
-        {
-          name : "Antimatter Condenser",
-          base : 170000000000000,
-          power: 430000000
-        },
-        {
-          name : "Prism",
-          base : 2100000000000000,
-          power: 2900000000
-        }
-    ];  
+var buildings = [
+  { name : "Hacker", base : power: .1 },
+  { name : "Grandma", base :  power: 1 },
+  { name : "Farm", base : 1100, power: 8 },
+  { name : "Mine", base : 12000, power: 47 },
+  { name : "Factory", base : 130000, power: 260 },
+  { name : "Bank", base : 1400000, power: 1400 },
+  { name : "Temple", base : 20000000, power: 7800 },
+  { name : "Wizard Tower", base : 330000000, power: 44000 },
+  { name : "Shipment", base : 5100000000, power: 260000 },
+  { name : "Alchemy Lab", base : 75000000000, power: 1600000 },
+  { name : "Portal", base : 1000000000000, power: 10000000 },
+  { name : "Time Machine", base : 14000000000000, power: 65000000 },
+  { name : "Antimatter Condenser", base : 170000000000000, power: 430000000 },
+  { name : "Prism", base : 2100000000000000, power: 2900000000}
+];
+
 var points = 0,
   gross = 0;
   freq = [];
