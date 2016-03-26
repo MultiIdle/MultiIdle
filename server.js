880a63900039b2831d68663602930c284d584b30
tonynater <tong.t.wang@gmail.com>
2016-03-25 22:21:05 -0700

    Setup mach 2

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
+app.use('/', express.static('static/public'));
+
+server.listen(4200, function(){
+  console.log('listening on *: 4200');
+});
